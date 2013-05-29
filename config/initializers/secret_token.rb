0e93b4dfe86cdd5148dd4f6a9346d012d4effd73
Alain Achkar <rails@alain.simplytech.com>
2013-05-29 00:48:07 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Handmadecrafts::Application.config.secret_token = '1cec43454e24387733fb1902e8a5d5a31e3b0b2ccb8408e1073b17a9541986d3ff8106da0014faa5e983013b6c7f66faac1a26dd28a204e9524e4eb2d62046f5'
