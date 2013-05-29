8e942fc25b4bab8e6656037403223e198d542463
Alain Achkar <rails@alain.simplytech.com>
2013-05-29 19:39:47 -0400

    added Events extension

---
+++ vendor/extensions/events/app/models/refinery/events/event.rb
@@ -0,0 +1,15 @@
+module Refinery
+  module Events
+    class Event < Refinery::Core::BaseModel
+      self.table_name = 'refinery_events'
+
+      attr_accessible :title, :date, :photo_id, :blurb, :position
+
+      acts_as_indexed :fields => [:title, :blurb]
+
+      validates :title, :presence => true, :uniqueness => true
+
+      belongs_to :photo, :class_name => '::Refinery::Image'
+    end
+  end
+end
